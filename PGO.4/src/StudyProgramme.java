5f16720b78cf844203dd43077b516ba180b87b17
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-16 12:19:27 +0200

    Update StudyProgramme.java

--- PGO.4/src/StudyProgramme.java
+++ PGO.4/src/StudyProgramme.java
@@ -1,18 +1,59 @@
+import java.util.List;
+
 public class StudyProgramme {
-    private String nazwaProgramu;
-    private String opisProgramu;
-    private int liczbaSemestrow;
-    private int maxItn;
+    private String studyProgrammeName;
+    private String codeOfStudyProgramme;
+    private int numberOfSemesters;
+    private int numberOfPossibleITNS;
+    private List<String> subjects;
 
-    public StudyProgramme(String nazwaProgramu, String opisProgramu, String liczbaSemestrów, int MaxItn) {
-        this.nazwaProgramu = nazwaProgramu;
-        this.opisProgramu = opisProgramu;
-        this.liczbaSemestrow = liczbaSemestrow;
-        this.maxItn = maxItn;
+    public StudyProgramme(String studyProgrammeName, String codeOfStudyProgramme, int numberOfSemesters, int numberOfPossibleITNS, List<String> subjects) {
+        this.studyProgrammeName = studyProgrammeName;
+        this.codeOfStudyProgramme = codeOfStudyProgramme;
+        this.numberOfSemesters = numberOfSemesters;
+        this.numberOfPossibleITNS = numberOfPossibleITNS;
+        this.subjects = subjects;
+    }
 
+    public String getStudyProgrammeName() {
+        return studyProgrammeName;
+    }
+
+    public void setStudyProgrammeName(String studyProgrammeName) {
+        this.studyProgrammeName = studyProgrammeName;
+    }
+
+    public String getCodeOfStudyProgramme() {
+        return codeOfStudyProgramme;
+    }
+
+    public void setCodeOfStudyProgramme(String codeOfStudyProgramme) {
+        this.codeOfStudyProgramme = codeOfStudyProgramme;
+    }
+
+    public int getNumberOfSemesters() {
+        return numberOfSemesters;
+    }
+
+    public void setNumberOfSemesters(int numberOfSemesters) {
+        this.numberOfSemesters = numberOfSemesters;
+    }
+
+    public int getNumberOfPossibleITNS() {
+        return numberOfPossibleITNS;
+    }
+
+    public void setNumberOfPossibleITNS(int numberOfPossibleITNS) {
+        this.numberOfPossibleITNS = numberOfPossibleITNS;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
     }
 
 
-
-    }
-
+}
